fe299bdb614fad32c24dbf6f3f31d1bddb5ddc81
Theo Coombes <theocoombes06@gmail.com>
2021-04-16 23:57:06 +0100

    fix?

--- core.py
+++ core.py
@@ -15,10 +15,10 @@ def init(url="http://localhost:8080", rsync_addr="user@0.0.0.0", rsync_dir="/pat
 
 def streamDownload(url):
     with requests.get(url, stream=True) as r:
-            r.raise_for_status()
-            with open("temp.gz", 'w+b') as f:
-                for chunk in r.iter_content(chunk_size=8192): 
-                    f.write(chunk)
+        r.raise_for_status()
+        with open("temp.gz", 'w+b') as f:
+            for chunk in r.iter_content(chunk_size=8192): 
+                f.write(chunk)
 
 # The main node instance.
 class __node:
