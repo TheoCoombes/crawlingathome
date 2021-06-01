2aaa367d21b9efa46c88aa1bf5d0068fe7f5f73f
Theo Coombes <theocoombes06@gmail.com>
2021-06-01 23:19:03 +0100

    add newline to print

--- version.py
+++ version.py
@@ -2,4 +2,4 @@ VERSION = "0.0.2"
 GITHUB = "https://github.com/TheoCoombes/crawlingathome"
 
 def PrintVersion():
-    print(f"Crawling@Home client v{VERSION}\nFor issues and source code, see {GITHUB}")
+    print(f"Crawling@Home client v{VERSION}\nFor issues and source code, see {GITHUB}\n")
