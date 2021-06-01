f70f25e2a14fe2016ede43cbb28a65510f392b14
Theo Coombes <theocoombes06@gmail.com>
2021-06-01 22:11:13 +0100

    Create version.py

---
+++ version.py
@@ -0,0 +1,5 @@
+VERSION = "0.0.2"
+GITHUB = "https://github.com/TheoCoombes/crawlingathome"
+
+def PrintVersion():
+    print(f"Crawling@Home client v{VERSION}\nFor issues and source code, see {GITHUB}")
