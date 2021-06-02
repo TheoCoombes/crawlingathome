77636c42423ecc468c634f45c02a8c4d54720853
Theo Coombes <theocoombes06@gmail.com>
2021-06-02 15:47:21 +0100

    fix merge

--- version.py
+++ version.py
@@ -2,4 +2,4 @@ VERSION = "1.1.0-pre"
 GITHUB = "https://github.com/TheoCoombes/crawlingathome"
 
 def PrintVersion():
-    print(f"Crawling@Home client v{VERSION}\nFor issues and source code, see {GITHUB}\n")
+    print(f"Crawling@Home client v{VERSION}\nFor issues and source code, see {GITHUB}\n\n")
