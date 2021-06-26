2a55f7027fde0dbca3142d757420f4b9496ad553
Theo Coombes <theocoombes06@gmail.com>
2021-06-26 20:13:03 +0100

    initial (unfinished) v1.4.0 commit

--- core.py
+++ core.py
@@ -7,6 +7,7 @@
 from requests import session
 from time import sleep
 import numpy as np
+import tarfile
 import shutil
 import gzip
 import os
@@ -15,12 +16,20 @@ from .errors import *
 
 
 # Creates and returns a new node instance.
-def init(url="http://crawlingathome.duckdns.org/", nickname=None):
-    return Client(url, nickname)
+def init(url="http://crawlingathome.duckdns.org/", nickname=None, type="Hybrid"):
+    t = type.lower()[0]
+    if t == "h":
+        return HybridClient(url, nickname)
+    elif t == "c":
+        return CPUClient(url, nickname)
+    elif t == "g":
+        return GPUClient(url, nickname)
+    else:
+        raise ValueError("[crawling@home] invalid worker `type`")
 
 
-# The main client instance.
-class Client:
+# The main 'hybrid' client instance.
+class HybridClient:
     def __init__(self, url, nickname, _recycled=False):
         if _recycled:
             return
@@ -32,11 +41,8 @@ class Client:
         self.url = url
 
         print("[crawling@home] connecting to crawling@home server...")
-        if nickname is None:
-            r = self.s.get(self.url + "api/new")
-        else:
-            payload = {"nickname": nickname}
-            r = self.s.get(self.url + "api/new", params=payload)
+        payload = {"nickname": nickname, "type": "HYBRID"}
+        r = self.s.get(self.url + "api/new", params=payload)
 
         if r.status_code != 200:
             try:
@@ -131,6 +137,7 @@ class Client:
     
     # Wrapper for `completeJob` (for older workers)
     def _markjobasdone(self, total_scraped : int):
+        print("[crawling@home] WARNING: avoid using `_markjobasdone(...)` and instead use `completeJob(...)` to mark a job as done.")
         self.completeJob(total_scraped)
         
 
@@ -158,7 +165,7 @@ class Client:
     
     # Returns True if the worker is still alive, otherwise returns False.
     def isAlive(self):
-        r = self.s.post(self.url + "api/validateWorker", json={"token": self.token})
+        r = self.s.post(self.url + "api/validateWorker", json={"token": self.token, type="HYBRID"})
 
         if r.status_code != 200:
             try:
@@ -173,4 +180,288 @@ class Client:
     # Removes the node instance from the server, ending all current jobs.
     def bye(self):
         self.s.post(self.url + "api/bye", json={"token": self.token})
-        print("[crawling@home] ended run")
+        print("[crawling@home] closed worker")
+
+        
+# The CPU client instance.
+# Programatically similar to `HybridClient`, with different completion functions.
+class CPUClient:
+    def __init__(self, url, nickname):
+        self.s = session()
+        self.url = url
+
+        print("[crawling@home] connecting to crawling@home server...")
+        payload = {"nickname": nickname, "type": "CPU"}
+        r = self.s.get(self.url + "api/new", params=payload)
+
+        if r.status_code != 200:
+            try:
+                self.log("Crashed", crashed=True)
+            except:
+                pass
+            raise ServerError(f"[crawling@home] Something went wrong, http response code {r.status_code}\n{r.text}\n")
+
+        print("[crawling@home] connected to crawling@home server")
+        data = r.json()
+        self.token = data["token"]
+        self.display_name = data["display_name"]
+        
+        print(f"[crawling@home] worker name: {self.display_name}")
+        print(f"\n\nYou can view this worker's progress at {self.url + 'worker/' + self.display_name}\n")
+    
+    
+    # Finds the amount of available jobs from the server, returning an integer.
+    def jobCount(self):
+        r = self.s.get(self.url + "api/jobCount")
+
+        if r.status_code != 200:
+            try:
+                self.log("Crashed", crashed=True)
+            except:
+                pass
+            raise ServerError(f"[crawling@home] Something went wrong, http response code {r.status_code}\n{r.text}\n")
+
+        count = int(r.text)
+        
+        print(f"[crawling@home] jobs remaining: {count}")
+
+        return count
+    
+    
+    # Makes the node send a request to the server, asking for a new job.
+    def newJob(self):
+        print("[crawling@home] looking for new job...")
+
+        r = self.s.post(self.url + "api/newJob", json={"token": self.token})
+
+        if r.status_code != 200:
+            try:
+                self.log("Crashed", crashed=True)
+            except:
+                pass
+            raise ServerError(f"[crawling@home] Something went wrong, http response code {r.status_code}\n{r.text}\n")
+        else:
+            data = r.json()
+            self.shard = data["url"]
+            self.start_id = np.int64(data["start_id"])
+            self.end_id = np.int64(data["end_id"])
+            self.shard_piece = data["shard"]
+            
+            print("[crawling@home] recieved new job")
+    
+    
+    # Downloads the current job's shard to the current directory (./shard.wat)
+    def downloadShard(self):
+        print("[crawling@home] downloading shard...")
+        self.log("Downloading shard", noprint=True)
+
+        with self.s.get(self.shard, stream=True) as r:
+            r.raise_for_status()
+            with open("temp.gz", 'w+b') as f:
+                for chunk in r.iter_content(chunk_size=8192): 
+                    f.write(chunk)
+        
+        with gzip.open('temp.gz', 'rb') as f_in:
+            with open('shard.wat', 'w+b') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+        
+        sleep(1) # Causes errors otherwise?
+        os.remove("temp.gz")
+
+        self.log("Downloaded shard", noprint=True)
+        print("[crawling@home] finished downloading shard")
+        
+    
+    # Uploads the image download URL for the GPU workers to use, marking the CPU job complete.
+    def completeJob(self, image_download_url : str):
+        r = self.s.post(self.url + "api/markAsDone-cpu", json={"token": self.token, "url": image_download_url})
+        print("[crawling@home] marked job as done")
+        
+        if r.status_code != 200:
+            try:
+                self.log("Crashed", crashed=True)
+            except:
+                pass
+            raise ServerError(f"[crawling@home] Something went wrong, http response code {r.status_code}\n{r.text}\n")
+    
+    
+    # Logs the string progress into the server.
+    def log(self, progress : str, crashed=False, noprint=False):
+        data = {"token": self.token, "progress": progress}
+
+        r = self.s.post(self.url + "api/updateProgress", json=data)
+        if not crashed and not noprint:
+            print(f"[crawling@home] logged new progress data: {progress}")
+
+        if r.status_code != 200 and not crashed:
+            try:
+                self.log("Crashed", crashed=True)
+            except:
+                pass
+            raise ServerError(f"[crawling@home] Something went wrong, http response code {r.status_code}\n{r.text}\n")
+    
+    
+    # Returns True if the worker is still alive, otherwise returns False.
+    def isAlive(self):
+        r = self.s.post(self.url + "api/validateWorker", json={"token": self.token, "type": "CPU"})
+
+        if r.status_code != 200:
+            try:
+                self.log("Crashed", crashed=True)
+            except:
+                pass
+            raise ServerError(f"[crawling@home] Something went wrong, http response code {r.status_code}\n{r.text}\n")
+        else:
+            return ("True" in r.text)
+    
+    
+    # Removes the node instance from the server, ending all current jobs.
+    def bye(self):
+        self.s.post(self.url + "api/bye", json={"token": self.token})
+        print("[crawling@home] closed worker")
+
+
+
+# The GPU client instance.
+class GPUClient:
+    def __init__(self, url, nickname):
+        if url[-1] != "/":
+            url += "/"
+        
+        self.s = session()
+        self.url = url
+
+        print("[crawling@home] connecting to crawling@home server...")
+        payload = {"nickname": nickname, "type": "GPU"}
+        r = self.s.get(self.url + "api/new", params=payload)
+
+        if r.status_code != 200:
+            try:
+                self.log("Crashed", crashed=True)
+            except:
+                pass
+            raise ServerError(f"[crawling@home] Something went wrong, http response code {r.status_code}\n{r.text}\n")
+
+        print("[crawling@home] connected to crawling@home server")
+        data = r.json()
+        self.token = data["token"]
+        self.display_name = data["display_name"]
+        
+        print(f"[crawling@home] worker name: {self.display_name}")
+        print(f"\n\nYou can view this worker's progress at {self.url + 'worker/' + self.display_name}\n")
+    
+    
+    # Finds the amount of available jobs from the server, returning an integer.
+    def jobCount(self):
+        r = self.s.get(self.url + "api/jobCount-gpu")
+
+        if r.status_code != 200:
+            try:
+                self.log("Crashed", crashed=True)
+            except:
+                pass
+            raise ServerError(f"[crawling@home] Something went wrong, http response code {r.status_code}\n{r.text}\n")
+
+        count = int(r.text)
+        
+        print(f"[crawling@home] GPU jobs remaining: {count}")
+
+        return count
+    
+    
+    # Makes the node send a request to the server, asking for a new job.
+    def newJob(self):
+        print("[crawling@home] looking for new job...")
+
+        r = self.s.post(self.url + "api/newJob-gpu", json={"token": self.token})
+
+        if r.status_code != 200:
+            try:
+                self.log("Crashed", crashed=True)
+            except:
+                pass
+            raise ServerError(f"[crawling@home] Something went wrong, http response code {r.status_code}\n{r.text}\n")
+        else:
+            data = r.json()
+            self.shard = data["url"]
+            self.start_id = np.int64(data["start_id"])
+            self.end_id = np.int64(data["end_id"])
+            self.shard_piece = data["shard"]
+            
+            print("[crawling@home] recieved new job")
+    
+    
+    # Downloads the CPU worker's processed images to a (new) ./images/ directory
+    def downloadShard(self):
+        print("[crawling@home] downloading uploaded data...")
+        self.log("Fetching images", noprint=True)
+
+        with self.s.get(self.shard, stream=True) as r:
+            r.raise_for_status()
+            with open("temp.tar", 'w+b') as f:
+                for chunk in r.iter_content(chunk_size=8192): 
+                    f.write(chunk)
+        
+        try:
+            os.mkdir("images")
+        except:
+            pass
+        
+        with tarfile.open('temp.tar', 'r') as tar:
+            tar.extractall("./images")
+        
+        sleep(1) # Causes errors otherwise?
+        os.remove("temp.tar")
+
+        self.log("Downloaded images", noprint=True)
+        print("[crawling@home] finished downloading data")
+        
+    
+    # Uploads the image download URL for the GPU workers to use, marking the CPU job complete.
+    def completeJob(self, total_scraped : int):
+        r = self.s.post(self.url + "api/markAsDone-gpu", json={"token": self.token, "count": total_scraped})
+        print("[crawling@home] marked job as done")
+        
+        if r.status_code != 200:
+            try:
+                self.log("Crashed", crashed=True)
+            except:
+                pass
+            raise ServerError(f"[crawling@home] Something went wrong, http response code {r.status_code}\n{r.text}\n")
+    
+    
+    # Logs the string progress into the server.
+    def log(self, progress : str, crashed=False, noprint=False):
+        data = {"token": self.token, "progress": progress}
+
+        r = self.s.post(self.url + "api/updateProgress", json=data)
+        if not crashed and not noprint:
+            print(f"[crawling@home] logged new progress data: {progress}")
+
+        if r.status_code != 200 and not crashed:
+            try:
+                self.log("Crashed", crashed=True)
+            except:
+                pass
+            raise ServerError(f"[crawling@home] Something went wrong, http response code {r.status_code}\n{r.text}\n")
+    
+    
+    # Returns True if the worker is still alive, otherwise returns False.
+    def isAlive(self):
+        r = self.s.post(self.url + "api/validateWorker", json={"token": self.token, "type": "GPU"})
+
+        if r.status_code != 200:
+            try:
+                self.log("Crashed", crashed=True)
+            except:
+                pass
+            raise ServerError(f"[crawling@home] Something went wrong, http response code {r.status_code}\n{r.text}\n")
+        else:
+            return ("True" in r.text)
+    
+    
+    # Removes the node instance from the server, ending all current jobs.
+    def bye(self):
+        self.s.post(self.url + "api/bye", json={"token": self.token})
+        print("[crawling@home] closed worker")
+        
