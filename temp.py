0eeef1c74d81461ab461b7660025f04d48f143d4
Theo Coombes <theocoombes06@gmail.com>
2021-08-13 19:44:14 +0100

    add back core functions

--- temp.py
+++ temp.py
@@ -1,13 +1,13 @@
 from requests import session
 
 from .errors import WorkerTimedOutError
-from .core import CPUClient
+from .core import CPUClient, printTS
 
 
 class TempCPUWorker:
     def __init__(self,
             url: str = "http://crawlingathome.duckdns.org/",
-            nickname: str = "anonymous"):
+            nickname: str = "anonymous") -> None:
         
         if url[-1] != "/":
             url += "/"
@@ -19,6 +19,8 @@ class TempCPUWorker:
         self.completed = 0
         
         self._c = CPUClient(self.url, self.nickname)
+        self.upload_address = self._c.upload_address
+        
         self.log("Waiting for new job...")
         
     
@@ -30,9 +32,39 @@ class TempCPUWorker:
             self.log(msg)
     
     
+    def downloadShard(self, path="") -> None:
+        print(f"{printTS()} downloading shard...")
+        self.log("Downloading shard")
+
+        with self.s.get(self.shard, stream=True) as r:
+            r.raise_for_status()
+            with open(path + "temp.gz", 'w+b') as f:
+                for chunk in r.iter_content(chunk_size=8192): 
+                    f.write(chunk)
+            
+        with gzip.open(path + 'temp.gz', 'rb') as f_in:
+            with open(path + 'shard.wat', 'w+b') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+            
+        sleep(1) # Causes errors otherwise?
+        os.remove(path + "temp.gz")
+
+        self.log("Downloaded shard")
+        print(f"{printTS()} finished downloading shard")
+    
+    
+    def updateUploadServer(self) -> None:
+        try:
+            self._c.updateUploadServer()
+            self.upload_address = self._c.upload_address
+        except WorkerTimedOutError:
+            self._c = CPUClient(self.url, self.nickname)
+            self.updateUploadServer()
+    
+    
     def newJob(self):
         while True:
-            wat = self.s.get(self.url + "custom/getCPUWAT").text
+            wat = self.s.get(self.url + "custom/get-cpu-wat").text
             if not "http" in wat:
                 print("[crawling@home] something went wrong when finding a job, breaking loop...")
                 self.log("Crashed.")
