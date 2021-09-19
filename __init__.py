1fd355abbfd3e7a7410877f1f88ff58486e06694
Theo Coombes <theocoombes06@gmail.com>
2021-09-19 15:01:11 +0100

    `FullWATClient`

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from .version import PrintVersion as _printversion
 _printversion()
 
 from .core import init, print, HybridClient, CPUClient, GPUClient
+from .temp import TempCPUWorker as FullWATClient
 from .recycler import dump, load
 from .version import VERSION as __version__
 from .errors import *
