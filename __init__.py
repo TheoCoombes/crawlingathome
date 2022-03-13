6eb348a7cd7b9ba8ebb58ef7ad9509f29b4cf2a4
Theo Coombes <theocoombes06@gmail.com>
2022-03-13 16:14:47 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,7 @@
 from .version import PrintVersion as _printversion
 _printversion()
 
-from .core import init, print, HybridClient, CPUClient, GPUClient
-from .temp import TempCPUWorker as FullWATClient
+from .core import init, print, Client
 from .recycler import dump, load
 from .version import VERSION as __version__
 from .errors import *
