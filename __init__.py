7db78db06a97b08b806960fcd2bb76b26cfc1fd4
Theo Coombes <theocoombes06@gmail.com>
2022-03-13 16:17:27 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,6 +2,5 @@ from .version import PrintVersion as _printversion
 _printversion()
 
 from .core import init, print, Client
-from .recycler import dump, load
 from .version import VERSION as __version__
 from .errors import *
