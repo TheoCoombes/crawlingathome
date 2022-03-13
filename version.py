ac4f2a7480a901a987afa319bebcb27b8b586cb7
Theo Coombes <theocoombes06@gmail.com>
2022-03-13 16:15:08 +0000

    Update version.py

--- version.py
+++ version.py
@@ -2,4 +2,4 @@ VERSION = "1.5.0"
 GITHUB = "https://github.com/TheoCoombes/crawlingathome"
 
 def PrintVersion():
-    print(f"Crawling@Home Client v{VERSION}\nFor issues and source code, see {GITHUB}\n\n")
+    print(f"Captions@Home Client v{VERSION}\nFor issues and source code, see {GITHUB}\n\n")
